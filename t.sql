3b3d044f51fc8c08e1bd778ddce46124cf4b10f6
Cosmos <chensiyuwilliam@hotmail.com>
2016-11-28 00:30:00 -0800

    added corner cases test

--- t.sql
+++ t.sql
@@ -1,4 +1,2 @@
-LOAD xlarge FROM 'xlarge.del' WITH INDEX
-SELECT COUNT(*) FROM xlarge
-SELECT * FROM xlarge WHERE key = 4240
-SELECT * FROM xlarge WHERE key > 400 AND key < 500 AND key > 100 AND key < 4000000
+LOAD movie FROM 'movie.del' WITH INDEX
+SELECT COUNT(*) FROM movie
